2ff33f5dd0e1baac59f654df27a91c141bd5be87
nathannolacio <nathannolacio04@gmail.com>
2024-11-01 20:27:42 -0300

    Conexao c/ bucket, geracao de logs

--- src/main/java/Conexao.java
+++ src/main/java/Conexao.java
@@ -4,47 +4,10 @@ import javax.sql.DataSource;
 
 public class Conexao {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/emap";
-    private static final String USER = "root";
-    private static final String PASSWORD = "";
-
+    private static final String URL = System.getenv("BD_URL");
+    private static final String USER = System.getenv("BD_USER");
+    private static final String PASSWORD = System.getenv("BD_PASSWORD");
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
-
-//    public static void main(String[] args) {
-////        String query = "SELECT * FROM usuario";
-//
-//        try (Connection connection = getConnection();
-////             Statement statement = connection.createStatement();
-////             ResultSet resultSet = statement.executeQuery(query);
-//        ) {
-//
-//            if (connection != null) {
-//                System.out.println("Conexão com o banco de dados estabelecida com sucesso!");
-//            }
-//
-////            while (resultSet.next()) {
-////                int id = resultSet.getInt("idUsuario");
-////                String nome = resultSet.getString("nomeUsuario");
-////                String cpf = resultSet.getString("cpf");
-////                String email = resultSet.getString("emailUsuario");
-////                String numCelular = resultSet.getString("numeroCelular");
-////                String senha = resultSet.getString("senha");
-////
-////                System.out.printf("""
-////                        ID: %d
-////                        Nome: %s
-////                        CPF: %s
-////                        Email: %s
-////                        Celular: %s
-////                        Senha: %s
-////                        """, id, nome, cpf, email, numCelular, senha);
-////            }
-//
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//
-//    }
 }
---
+++ src/main/java/ConexaoBucket.java
@@ -0,0 +1,32 @@
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.BasicSessionCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+public class ConexaoBucket {
+    private static AmazonS3 s3Client;
+
+    static {
+        String accessKey = System.getenv("AWS_ACCESS_KEY_ID");
+        String secretKey = System.getenv("AWS_SECRET_ACCESS_KEY");
+        String sessionToken = System.getenv("AWS_SESSION_TOKEN");
+        String region = System.getenv("AWS_DEFAULT_REGION");
+
+        if (accessKey == null || secretKey == null || region == null || sessionToken == null) {
+            throw new IllegalStateException("As variáveis de ambiente AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_DEFAULT_REGION e AWS_SESSION_TOKEN devem estar definidas.");
+        }
+
+        BasicSessionCredentials sessionCredentials = new BasicSessionCredentials(accessKey, secretKey, sessionToken);
+
+        s3Client = AmazonS3ClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(sessionCredentials))
+                .withRegion(region)
+                .build();
+    }
+
+    public static AmazonS3 getS3Client() {
+        return s3Client;
+    }
+
+}
