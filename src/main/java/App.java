c45063acfc7ae4d46ec4c59f37c6873aa64e96c1
nathannolacio <nathannolacio04@gmail.com>
2024-11-19 18:23:46 -0300

    Integracao Slack

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,10 +1,6 @@
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.util.List;
 
 public class Main {
-    private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
 
